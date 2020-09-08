a26b3f7cb9a0d4ed2bdd0b5f417650a222679795
Bence Losonczy <losonzybence@gmail.com>
2020-09-08 16:40:00 +0200

    Majority Element done

---
+++ src/main/java/com/codecool/ctci/MajorityElement.java
@@ -0,0 +1,23 @@
+package com.codecool.ctci;
+
+public class MajorityElement {
+
+    int findMajorityElement(int[] array){
+        for(int x : array){
+            if (validate(array,x)){
+                return x;
+            }
+        }
+        return -1;
+    }
+
+    boolean validate(int[] array, int majority){
+        int count = 0 ;
+        for (int n : array){
+            if (n == majority){
+                count++
+            }
+        }
+        return count > array.length/2;
+    }
+}
